36c9afbbca6f27bd87fa2474d164f143d87d670a
SudhaSanthakumar <sudha.santhakumar@gmail.com>
2022-02-08 13:10:57 -0800

    Updated controller and appl.prop

--- src/main/java/com/example/docker/DockerApplication.java
+++ src/main/java/com/example/docker/DockerApplication.java
@@ -8,26 +8,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerApplication {
 
 	
-	@Autowired
-	private MovieRepository mRepo;
+//	@Autowired
+//	private MovieRepository mRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DockerApplication.class, args);
 	}
 	
-	public void run(String... args) throws Exception{
-		
-		mRepo.save(new Movie("Bahubali"));
-		
-		mRepo.save(new Movie("Sura"));
-		
-		mRepo.save(new Movie("Sarkar"));
-		
-		mRepo.save(new Movie("Thalaiva"));
-		
-		mRepo.save(new Movie("Singam"));
-		
-		
-	}
+//	public void run(String... args) throws Exception{
+//		
+//		mRepo.save(new Movie("Bahubali"));
+//		
+//		mRepo.save(new Movie("Sura"));
+//		
+//		mRepo.save(new Movie("Sarkar"));
+//		
+//		mRepo.save(new Movie("Thalaiva"));
+//		
+//		mRepo.save(new Movie("Singam"));
+//		
+//		
+//	}
 
 }
--- src/main/java/com/example/docker/MovieController.java
+++ src/main/java/com/example/docker/MovieController.java
@@ -1,7 +1,9 @@
 package com.example.docker;
 
 import java.util.List;
+import java.util.logging.LogManager;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +21,14 @@ public class MovieController {
 	@Autowired
 	MovieRepository mRepo;
 	
+	
+	
 	@GetMapping("/movies")
 	public List<Movie> getMovies() {
-		return mRepo.findAll();
+		System.out.println("inside get movies");
+		List<Movie> mov=mRepo.findAll();
+		System.out.println(mov.toString());
+		return mov ;
 	}
 	
 	@PostMapping("/movies")
